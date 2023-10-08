b51a2bdaae598ebfef90907abe9d1b6eca4290ff
Anant-raj2 <anant.raj@franklinsabers.org>
2023-10-07 23:59:55 -0500

    worked onf form handling and HERO page

--- js/home.js
+++ js/home.js
@@ -1,10 +1,51 @@
-document.getElementById("getStartedBtn").addEventListener("click", function () {
-  document.body.classList.toggle("zoomed-out");
-  setTimeout(function () {
+document.addEventListener("DOMContentLoaded", function () {
+  // Your code here
+  document.getElementById("getStartedButton").addEventListener("click", () => {
+    console.log("Button Clicked");
     window.location.href = "form.html";
-  }, 500);
+  });
 });
 
-document.getElementById("logoHomeBtn").addEventListener("click", function () {
-  window.location.href = "home.html";
-});
+const phrases = ["Collaborate", "Connect", "Innovate", "Network"];
+let phraseIndex = 0;
+let charIndex = 0;
+const typingSpeed = 150;
+const deletingSpeed = 100;
+const delayBetweenPhrases = 50000;
+let isDeleting = false;
+const changingPhraseElement = document.getElementById("changingPhrase");
+
+function typePhrase() {
+  if (isDeleting) {
+    changingPhraseElement.textContent = phrases[phraseIndex].substr(
+      0,
+      charIndex - 1
+    );
+    charIndex--;
+
+    if (charIndex <= 0) {
+      isDeleting = false;
+      phraseIndex = (phraseIndex + 1) % phrases.length;
+    }
+  } else {
+    changingPhraseElement.textContent = phrases[phraseIndex].substr(
+      0,
+      charIndex + 1
+    );
+    charIndex++;
+
+    if (charIndex >= phrases[phraseIndex].length) {
+      isDeleting = true;
+    }
+  }
+
+  let timeout = isDeleting ? deletingSpeed : typingSpeed;
+  if (!isDeleting && charIndex === phrases[phraseIndex].length) {
+    timeout += delayBetweenPhrases;
+  }
+
+  setTimeout(typePhrase, timeout);
+}
+
+// Start the typing effect
+typePhrase();
